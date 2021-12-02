b3c0fa839e953c27854c6320f83687e9ea7f0423
Kevin Olentino <kevinolentino@gmail.com>
2021-12-02 14:03:28 -0400

    Initial commit

---
+++ ControleBovideoSquadFrontEnd/Controllers/MunicipioController.cs
@@ -0,0 +1,15 @@
+﻿using ControleBovideoSquad.CrossCutting.Dto.Municipio;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace ControleBovideoSquadFrontEnd.Controllers
+{
+    public class MunicipioController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {           
+            return View();
+        }
+    }
+}
---
+++ ControleBovideoSquadFrontEnd/Controllers/ProdutorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleBovideoSquadFrontEnd.Controllers
+{
+    public class ProdutorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
